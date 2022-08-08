327799fdbaf8a7ef4c9abf51c94f6dda703f839a
Darwin Luque <darwin.luque.98@gmail.com>
2022-08-08 09:00:38 -0600

    wip: add casl module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormconfig } from './config/typeorm/ormconfig';
 import { AuthModule } from './components/auth/auth.module';
+import { CaslModule } from './casl/casl.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), AuthModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), AuthModule, CaslModule],
   controllers: [],
   providers: [],
 })
