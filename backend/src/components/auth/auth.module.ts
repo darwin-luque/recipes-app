b14a5326b6536b80b90fa3ae623ae2255e52985a
Darwin Luque <darwin.luque.98@gmail.com>
2022-08-08 23:33:47 -0600

    fix(backend-auth): import passport module and jwt module

--- backend/src/components/auth/auth.module.ts
+++ backend/src/components/auth/auth.module.ts
@@ -1,12 +1,27 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { CqrsModule } from '@nestjs/cqrs';
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 import { User } from '../../infrastructure/entities/user.entity';
 import { CommandHandlers } from './commands/handlers';
 import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [CqrsModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    CqrsModule,
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_TOKEN_SECRET,
+      signOptions: {
+        audience: process.env.JWT_AUDIENCE,
+        issuer: process.env.JWT_ISSUER,
+        expiresIn: process.env.JWT_EXPIRATION_TIME,
+        algorithm: 'HS256',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [...CommandHandlers],
 })
