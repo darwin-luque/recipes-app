499d81f4064b6157634d02e501d173cf8d2610b3
Darwin Luque <darwin.luque.98@gmail.com>
2022-08-08 23:26:40 -0600

    chore: mark sign up route as public

--- backend/src/components/auth/auth.controller.ts
+++ backend/src/components/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Public } from '../../infrastructure/decorators/public.decorator';
 import { SignUpCommand } from './commands/sign-up';
 import { SignUpDto } from './dtos/sign-up.dto';
 
@@ -11,6 +12,7 @@ export class AuthController {
   ) {}
 
   @Post('sign-up')
+  @Public()
   signup(@Body() body: SignUpDto) {
     return this.commandBus.execute(new SignUpCommand(body));
   }
