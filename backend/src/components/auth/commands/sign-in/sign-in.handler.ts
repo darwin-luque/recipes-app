8ba496ede6ffdfbadeb57f9a435cec9bf49d755f
Darwin Luque <darwin.luque.98@gmail.com>
2022-08-20 11:37:52 -0600

    chore: return user and token from sign in handler

--- backend/src/components/auth/commands/sign-in/sign-in.handler.ts
+++ backend/src/components/auth/commands/sign-in/sign-in.handler.ts
@@ -1,18 +1,22 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 import { User } from '../../../../infrastructure/entities/user.entity';
 import { SignInCommand } from './sign-in.command';
+import { dumpTokenPayload } from '../../auth.helpers';
+import { UserToken } from '../../auth.types';
 
 @CommandHandler(SignInCommand)
 export class SignInHandler implements ICommandHandler<SignInCommand> {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
 
-  async execute(command: SignInCommand): Promise<User> {
+  async execute(command: SignInCommand): Promise<UserToken> {
     let where = {};
 
     if (command.data.username) {
@@ -29,11 +33,13 @@ export class SignInHandler implements ICommandHandler<SignInCommand> {
     }
 
     const isValid = await bcrypt.compare(command.data.password, user.password);
+    const tokenPayload = dumpTokenPayload(user);
+    const token = await this.jwtService.signAsync(tokenPayload);
 
     if (!isValid) {
       throw new UnauthorizedException('Invalid password');
     }
 
-    return user;
+    return { user, token };
   }
 }
