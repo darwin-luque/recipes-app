adb5797472a3ae189d7c37ff514e5469a2987800
Darwin Luque <darwin.luque.98@gmail.com>
2022-08-20 19:11:22 -0600

    feat(backend-auth): add a me route

--- backend/src/components/auth/auth.controller.ts
+++ backend/src/components/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CurrentUser } from '../../infrastructure/decorators/current-user.decorator';
 import { Public } from '../../infrastructure/decorators/public.decorator';
 import { Serialize } from '../../infrastructure/decorators/serialize.decorator';
+import { User } from '../../infrastructure/entities/user.entity';
 import { UserToken } from './auth.types';
 import { SignInCommand } from './commands/sign-in';
 import { SignUpCommand } from './commands/sign-up';
 import { SignInDto } from './dtos/sign-in.dto';
 import { SignUpDto } from './dtos/sign-up.dto';
 import { UserTokenDto } from './dtos/user-token.dto';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -28,5 +31,11 @@ export class AuthController {
   @Serialize(UserTokenDto)
   signin(@Body() body: SignInDto): Promise<UserToken> {
     return this.commandBus.execute(new SignInCommand(body));
+  }
+
+  @Get('me')
+  @Serialize(UserDto)
+  me(@CurrentUser() user: User): User {
+    return user;
   }
 }
